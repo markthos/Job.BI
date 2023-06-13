33ed3115f23e13d0fbc0926bf87c546be13b2467
Your Name <you@example.com>
2023-06-12 17:58:57 -0700

    first commit

--- assets/landingPage.js
+++ assets/landingPage.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const loginLink = document.getElementById("login");
     const signupLink = document.getElementById("signup");
     const loginForm = document.querySelector(".form-box.login");
-    const signupForm = document.querySelector(".form-box.signup");
-  
+    const signupForm =  document.querySelector(".form-box.signup");
+
     loginForm.style.display = "none";
     signupForm.style.display = "none"; 
+
 
     loginLink.addEventListener("click", function(event) {
       event.preventDefault();
