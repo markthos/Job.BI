a42e1673a333a868e0bf1d1cbc0744f29f9f3a66
Your Name <you@example.com>
2023-06-13 23:14:00 -0700

    make changes

--- assets/landingPage.js
+++ assets/landingPage.js
@@ -8,29 +8,29 @@ document.addEventListener("DOMContentLoaded", function() {
     signupForm.style.display = "none"; 
 
 
-loginLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    loginForm.style.display = "block";
-    signupForm.style.display = "none";
-  });
-
-  signupLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    loginForm.style.display = "none";
-    signupForm.style.display = "block";
-  });
-
-  loginForm.addEventListener("click", function(event) {
-      event.preventDefault();
-      loginForm.style.display = "none";
-      signupForm.style.display = "block";
-
-    });
-  
-    signupForm.addEventListener("click", function(event) {
+    loginLink.addEventListener("click", function(event) {
       event.preventDefault();
       loginForm.style.display = "block";
       signupForm.style.display = "none";
-  });
+    });
+  
+    signupLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      loginForm.style.display = "none";
+      signupForm.style.display = "block";
+    });
 
-});
+    loginForm.addEventListener("click", function(event) {
+        event.preventDefault();
+        loginForm.style.display = "none";
+        signupForm.style.display = "block";
+
+      });
+    
+      signupForm.addEventListener("click", function(event) {
+        event.preventDefault();
+        loginForm.style.display = "block";
+        signupForm.style.display = "none";
+
+      });
+    });
