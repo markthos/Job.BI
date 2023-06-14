dfc3920f4dc385c1644d481bcf7dae7a026f3f4a
Your Name <you@example.com>
2023-06-13 17:44:26 -0700

    make changes

--- assets/landingPage.js
+++ assets/landingPage.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const signupLink = document.getElementById("signup");
     const loginForm = document.querySelector(".form-box.login");
     const signupForm =  document.querySelector(".form-box.signup");
-
-    loginForm.style.display = "none";
+ 
+   loginForm.style.display = "none";
     signupForm.style.display = "none"; 
 
 
@@ -19,8 +19,20 @@ document.addEventListener("DOMContentLoaded", function() {
       loginForm.style.display = "none";
       signupForm.style.display = "block";
     });
-  });
 
+    loginForm.addEventListener("click", function(event) {
+        event.preventDefault();
+        loginForm.style.display = "none";
+        signupForm.style.display = "block";
 
+      });
+    
+      signupForm.addEventListener("click", function(event) {
+        event.preventDefault();
+        loginForm.style.display = "block";
+        signupForm.style.display = "none";
 
-
+      });
+    
+    
+    });
