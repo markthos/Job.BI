69e6299a0bc398ca0b98c6d94a6b94f794bec5a3
Your Name <you@example.com>
2023-06-14 21:48:18 -0700

    make changes

--- assets/landingPage.js
+++ assets/landingPage.js
@@ -20,6 +20,38 @@ signupLink.addEventListener("click", function(event) {
     signupForm.style.display = "block";
   });
 
+  // Handle loginForm submit
+  const loginFormContainer = document.getElementById("loginForm");
+  loginForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Make an API request to authenticate the user
+    // Replace 'API_ENDPOINT' with your URL of your API endpoint for login
+    fetch("/api/v2/clientAwareMemberHandles", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json" }
+    })
+    body: JSON.stringify({
+         username: username,
+         password: password
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle the API response
+        if (data.success) {
+           // Login successful, perfom necessary actions (e.g. redirect, display success message)
+           console.log("Login successful");
+        } else {
+            //Login failed, display error message or handle other error scenarios
+          console.log("Login failed");
+        }
+      });
+  });
+
 
   // Add an event listener to the login button
   document.getElementById("loginBtn").addEventListener("click", function(event) {
