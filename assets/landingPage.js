68d7de6e496102b91311972bac3697fe54b82cc0
Your Name <you@example.com>
2023-06-17 00:12:54 -0700

    changes

--- assets/landingPage.js
+++ assets/landingPage.js
@@ -1,71 +1,167 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var loginLink = document.getElementById("login");
-  var signupLink = document.getElementById("signup");
-  var loginForm = document.querySelector(".form-box.login");
-  var signupForm =  document.querySelector(".form-box.signup");
-  // const name = document.getElementById("name");
-  // const email = document.getElementById("email");
-  // const password = document.getElementById("password");
-  // const form = document.getElementById("form");
+// document.addEventListener("DOMContentLoaded", function() {
+//   var loginLink = document.getElementById("login");
+//   var signupLink = document.getElementById("signup");
+//   var loginForm = document.querySelector(".form-box.login");
+//   var signupForm =  document.querySelector(".form-box.signup");
+//   // const name = document.getElementById("name");
+//   // const email = document.getElementById("email");
+//   // const password = document.getElementById("password");
+//   // const form = document.getElementById("form");
 
-//   // Create var user data
-// var user = {
-//   username: 'Mama Mo',
-//   email: 'monique.merzoug@icloud.com',
-//   password: '123456789',
+// //   // Create var user data
+// // var user = {
+// //   username: 'Mama Mo',
+// //   email: 'monique.merzoug@icloud.com',
+// //   password: '123456789',
+// // };
+
+//   loginForm.style.display = "none";
+//   signupForm.style.display = "none"; 
+
+
+// loginLink.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   loginForm.style.display = "block";
+//    signupForm.style.display = "none";
+
+//  });
+
+//  signupLink.addEventListener("click", function(event) {
+//    event.preventDefault();
+//    loginForm.style.display = "none";
+//    signupForm.style.display = "block";
+
+//  });
+
+
+// });
+
+// // Define const user data
+// var userData = {
+//   "name": "John Doe",
+//   "email": "johndoe@example.com",
+//   "password": "password123"
 // };
 
-  loginForm.style.display = "none";
-  signupForm.style.display = "none"; 
+// //  // // Store user data in local storage
+//  localStorage.setItem("userData", JSON.stringify(userData));
+
+// // // Retrieve user data from local storage
+// var retrievedUser = JSON.parse(localStorage.getItem("userData"));
+
+// // Function to update the HTML with user data
+// var displayUserData = (user) => {
+//   var name = document.getElementById("name");
+//   var email = document.getElementById("email");
+//   var password = document.getElementById("password");
+//   var form = document.getElementById("form");
+
+//   name.innerText = user.name;
+//   email.innerText = user.email;
+//   password.innerText = user.password;
+// };
+
+// // Call the function to update the HTML
+//  displayUserData(retrievedUser);
+
+// Event listeners for the login and signup buttons
+document.getElementById("login").addEventListener("click", showLoginForm);
+document.getElementById("signup").addEventListener("click", showSignupForm);
+
+// Event listener for the login form submission
+document.getElementById("loginBtn").addEventListener("click", handleLogin);
+
+// Event listener for the signup form submission
+document.getElementById("submitBtn").addEventListener("click", handleSignup);
+
+// Function to hide the signup and login forms initially
+function hideForms() {
+  document.getElementById("loginForm").style.display = "none";
+  document.getElementById("signupForm").style.display = "none";
+}
+
+// Function to show the login form
+function showLoginForm() {
+  document.getElementById("loginForm").style.display = "block";
+  document.getElementById("signupForm").style.display = "none";
+}
+
+// Function to show the signup form
+function showSignupForm() {
+  document.getElementById("loginForm").style.display = "none";
+  document.getElementById("signupForm").style.display = "block";
+}
+
+// Function to validate the login credentials
+function isValidCredentials(username, password) {
+  // You can customize this function to perform your desired validation logic
+  // For example, you can check if the username and password match the stored values in localStorage
+  var storedUsername = localStorage.getItem("username");
+  var storedPassword = localStorage.getItem("password");
+
+  return username === storedUsername && password === storedPassword;
+}
+
+// Function to handle the login form submission
+function handleLogin(event) {
+  event.preventDefault(); // Prevent form submission
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Perform login validation and redirection
+  if (isValidCredentials(username, password)) {
+    // Store the credentials in localStorage
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    alert("You are successfully logged in");
+    window.location.href = "http://127.0.0.1:5500/profilePage.html"; // Redirect to profile page
+  } else {
+    alert("Invalid username or password");
+  }
+}
+
+// Function to handle the signup form submission
+function handleSignup(event) {
+  event.preventDefault(); // Prevent form submission
+
+  var username = document.getElementById("usernameInput").value;
+  var email = document.getElementById("emailInput").value;
+  var password = document.getElementById("passwordInput").value;
+
+  // Store the signup information in localStorage or perform any desired actions
+  localStorage.setItem("username", username);
+  localStorage.setItem("email", email);
+  localStorage.setItem("password", password);
+
+  alert("Signup successful. You can now login with your credentials.");
+  // Redirect to another page if needed
+  window.location.href = "http://127.0.0.1:5500/profilePage.html";
+}
+
+// Function to check if the user is already logged in
+function checkLoggedIn() {
+  var loggedIn = localStorage.getItem("loggedIn");
+  if (loggedIn === "true") {
+    window.location.href = "http://127.0.0.1:5500/profilePage.html";
+  }
+}
+
+// Function to initialize the page
+function initializePage() {
+  hideForms();
+  checkLoggedIn();
+}
+
+// Event listeners for the login and signup buttons
+document.getElementById("login").addEventListener("click", showLoginForm);
+document.getElementById("signup").addEventListener("click", showSignupForm);
+
+// Call the initializePage function when the page loads
+window.addEventListener("load", initializePage);
 
 
-loginLink.addEventListener("click", function(event) {
-  event.preventDefault();
-  loginForm.style.display = "block";
-   signupForm.style.display = "none";
-
- });
-
- signupLink.addEventListener("click", function(event) {
-   event.preventDefault();
-   loginForm.style.display = "none";
-   signupForm.style.display = "block";
-
- });
-
-
-});
-
-// Define const user data
-var userData = {
-  "name": "John Doe",
-  "email": "johndoe@example.com",
-  "password": "password123"
-};
-
-//  // // Store user data in local storage
- localStorage.setItem("userData", JSON.stringify(userData));
-
-// // Retrieve user data from local storage
-var retrievedUser = JSON.parse(localStorage.getItem("userData"));
-
-// Function to update the HTML with user data
-var displayUserData = (user) => {
-  var name = document.getElementById("name");
-  var email = document.getElementById("email");
-  var password = document.getElementById("password");
-  var form = document.getElementById("form");
-
-  name.innerText = user.name;
-  email.innerText = user.email;
-  password.innerText = user.password;
-};
-
-// Call the function to update the HTML
- displayUserData(retrievedUser);
-
- // Function to update the HTML
- 
 
 //  // Save user data to local storage
 // localStorage.setItem('user', JSON.stringify(user));
