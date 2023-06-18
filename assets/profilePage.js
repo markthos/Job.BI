826d7b1723e8160ec4d7af54cb49d7abd86b8d68
Mark T <aydenthos@gmail.com>
2023-06-18 01:10:39 -0700

    removed email option in login button

--- assets/profilePage.js
+++ assets/profilePage.js
@@ -131,22 +131,22 @@ skipBtn.addEventListener('click', function() {
     updateFormContainer.style.display = 'none';
 });
 
-var searchitem = document.getElementById ("get-quote")
-searchitem.addEventListener ("click",getQuotesList)
+// var searchitem = document.getElementById ("get-quote")
+// searchitem.addEventListener ("click",getQuotesList)
 
-window.onload = getQuotesList
+// window.onload = getQuotesList
 
 
-function getQuotesList(e){
+// function getQuotesList(e){
 
-    fetch("https://type.fit/api/quotes")
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) { 
-        console.log(data);
-        console.log(data.length)
-        var ind = Math.floor(Math.random() * data.length)
-        document.getElementById("quote").innerText = data[ind].text + " ~~~"+data[ind].author
-      });
-}
+//     fetch("https://type.fit/api/quotes")
+//       .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(data) { 
+//         console.log(data);
+//         console.log(data.length)
+//         var ind = Math.floor(Math.random() * data.length)
+//         document.getElementById("quote").innerText = data[ind].text + " ~~~"+data[ind].author
+//       });
+// }
