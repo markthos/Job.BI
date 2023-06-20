8152dc99f8b586ccdc9362c358d04b5e10a43c75
Mark T <aydenthos@gmail.com>
2023-06-19 18:14:10 -0700

    fixed the Login and Authentication

--- assets/landingPage.js
+++ assets/landingPage.js
@@ -1,24 +1,24 @@
-// functionality of the inspirational quote
-var searchitem = document.getElementById ("get-quote")
-searchitem.addEventListener ("click",getQuotesList)
+// Function for inspirational quote
+var searchitem = document.getElementById("get-quote");
+searchitem.addEventListener("click", getQuotesList);
 
-window.onload = getQuotesList
+window.onload = getQuotesList;
 setInterval(getQuotesList, 15000); // Change quote every 15 seconds
-function getQuotesList(e){
 
+function getQuotesList(e) {
   fetch("https://type.fit/api/quotes")
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) { 
+    .then(function (data) {
       console.log(data);
-      console.log(data.length)
-      var ind = Math.floor(Math.random() * data.length)
-      document.getElementById("quote").innerText = data[ind].text + " ~~~"+data[ind].author
+      console.log(data.length);
+      var ind = Math.floor(Math.random() * data.length);
+      document.getElementById("quote").innerText = data[ind].text + " ~~~" + data[ind].author;
     });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var loginLink = document.getElementById("login");
   var signupLink = document.getElementById("signup");
   var loginForm = document.querySelector(".form-box.login");
@@ -27,64 +27,66 @@ document.addEventListener("DOMContentLoaded", function() {
   loginForm.style.display = "none";
   signupForm.style.display = "none";
 
-  loginLink.addEventListener("click", function(event) {
+  loginLink.addEventListener("click", function (event) {
     event.preventDefault();
     loginForm.style.display = "block";
     signupForm.style.display = "none";
   });
 
-  signupLink.addEventListener("click", function(event) {
+  signupLink.addEventListener("click", function (event) {
     event.preventDefault();
     loginForm.style.display = "none";
     signupForm.style.display = "block";
   });
 
-  document.getElementById("loginBtn").addEventListener("click", function(event) {
-    event.preventDefault();
-    // Perform login validation and other necessary checks here
-    // If login is successful, redirect the user to the profile page
-    var passwordInput = document.getElementById("loginPassword");
-    var usernameInput = document.getElementById("loginUsername");
-    
+  function auth() {
+    var usernameInput = document.getElementById("username");
+    var passwordInput = document.getElementById("password");
+
     var username = usernameInput.value.trim();
     var password = passwordInput.value.trim();
-  
+
     // Retrieve user data from local storage
     var user = JSON.parse(localStorage.getItem("user"));
-  
+
     if (user && user.username === username && user.password === password) {
-      window.location.href = "http://127.0.0.1:5500/profilePage.html";
+      // Redirect the user to the profile page
+      window.location.href = "profilePage.html";
     } else {
       console.log("Invalid username or password. Please try again.");
     }
-  });
-  
+  }
 
-  document.getElementById("signupBtn").addEventListener("click", function(event) {
+  document.getElementById("loginBtn").addEventListener("click", function (event) {
     event.preventDefault();
-  
+    auth(); // Call the auth function for login authentication
+  });
+
+  document.getElementById("signupBtn").addEventListener("click", function (event) {
+    event.preventDefault();
+
     // Perform signup validation and other necessary checks here
-  
+
     var usernameInput = document.getElementById("usernameInput");
     var emailInput = document.getElementById("emailInput");
     var passwordInput = document.getElementById("passwordInput");
-  
+
     var username = usernameInput.value.trim();
     var email = emailInput.value.trim();
     var password = passwordInput.value.trim();
-  
+
     var user = {
       username: username,
       email: email,
       password: password,
     };
-  
+
     // Encode the user object as a query parameter in the URL
     var queryString = "?user=" + encodeURIComponent(JSON.stringify(user));
-  
+
     // Redirect the user to the profile page with the query parameter
     window.location.href = "http://127.0.0.1:5500/profilePage.html" + queryString;
-  
+
     // Clear the form fields on clicking the signup button
     usernameInput.value = "";
     emailInput.value = "";
